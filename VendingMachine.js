081499472045df543e8b6755b77bed0ac361bbe2
morita657 <morita657@gmail.com>
2018-04-10 15:42:44 +0900

    Add error message if not enough coins inserted

--- VendingMachine.js
+++ VendingMachine.js
@@ -43,6 +43,76 @@ class VendingMachine {
   _selectColumn(column) {
     this.selection.column = column;
     console.log(this.selection.row, column);
+    this._vend();
+  }
+
+  _vend() {
+    const buttonMap = {
+      A: 0,
+      B: 1,
+      C: 2,
+      D: 3,
+    };
+    const row = buttonMap[this.selection.row];
+    const column = this.selection.column - 1;
+
+    if (this.inventory[row][column].price <= this.balance) {
+      this.inventory[row][column].count--;
+      console.log("Here is your " + this.inventory[row][column].name);
+
+      this._dispenseChange(this.inventory[row][column].price);
+    } else {
+      // give error
+      console.error("Not enough money.");
+    }
+  }
+
+  _dispenseChange(price) {
+    let change = this.balance - price;
+
+    const returnedCoins = this._calculateChange(change);
+
+    console.log(returnedCoins);
+
+    this._initialise();
+  }
+
+  _calculateChange(change) {
+    const returnedCoins = {
+      10: 0,
+      50: 0,
+      100: 0,
+      500: 0,
+    };
+
+    while (change >= 500 && this.till[500] > 0) {
+      returnedCoins[500]++;
+      this.till[500]--;
+      change -= 500;
+    }
+    while (change >= 100 && this.till[100] > 0) {
+      returnedCoins[100]++;
+      this.till[100]--;
+      change -= 100;
+    }
+    while (change >= 50 && this.till[50] > 0) {
+      returnedCoins[50]++;
+      this.till[50]--;
+      change -= 50;
+    }
+    while (change > 0 && this.till[10] > 0) {
+      returnedCoins[10]++;
+      this.till[10]--;
+      change -= 10;
+    }
+
+    return returnedCoins;
+  }
+
+  _initialise() {
+    this.selection.row = undefined;
+    this.selection.column = undefined;
+    this.coinsInserted = [];
   }
 }
 
